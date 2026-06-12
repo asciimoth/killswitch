aaec5d409e20d0c78af99e67faeadd598ce9fb33
AsciiMoth <ascii@moth.contact>
2026-06-12 18:25:29 +0400

    feat: add nixos service

--- flake.nix
+++ flake.nix
@@ -1,3 +1,19 @@
+# Usage:
+# - Build the package: nix build .#killswitch
+# - Add to tmp shell: nix shell .#killswitch
+# - Add to profile: nix profile add .#killswitch
+# - Install the package in a NixOS config:
+#     environment.systemPackages = [ inputs.killswitch.packages.${pkgs.system}.killswitch ];
+# - Enable the NixOS daemon service from this flake:
+#     imports = [ inputs.killswitch.nixosModules.killswitch ];
+#     services.killswitch = {
+#       enable = true;
+#       settings = {
+#        interface_regexps = [ "^(en|wl|ww)" ];
+#        enable_v4 = true;
+#        allowed_v4_hostports = [ "udp/198.51.100.10:51820" ];
+#       };
+#     };
 {
   description = "Linux network killswitch";
   inputs = {
@@ -16,7 +32,75 @@
     flake-utils,
     pre-commit-hooks,
     ...
-  }:
+  }: let
+    module = {
+      config,
+      lib,
+      pkgs,
+      ...
+    }: let
+      cfg = config.services.killswitch;
+      jsonFormat = pkgs.formats.json {};
+      settingsFile = jsonFormat.generate "killswitch.json" cfg.settings;
+      configPath =
+        if cfg.settings == null
+        then cfg.configPath
+        else settingsFile;
+    in {
+      options.services.killswitch = {
+        enable = lib.mkEnableOption "Linux eBPF network killswitch daemon";
+
+        package = lib.mkPackageOption self.packages.${pkgs.stdenv.hostPlatform.system} "killswitch" {};
+
+        settings = lib.mkOption {
+          type = lib.types.nullOr jsonFormat.type;
+          default = null;
+          example = lib.literalExpression ''
+            {
+              interface_regexps = [ "^(en|wl|ww)" ];
+              ignored_interface_regexps = [ "^(veth|br-)" ];
+              admin_api.auth.groupnames = [ "killswitch" ];
+              enable_v4 = true;
+              allowed_v4_hostports = [ "udp/198.51.100.10:51820" ];
+            }
+          '';
+          description = ''
+            Killswitch daemon configuration rendered as JSON. When this is set,
+            it takes precedence over `configPath`.
+          '';
+        };
+
+        configPath = lib.mkOption {
+          type = lib.types.path;
+          default = "/etc/killswitch/killswitch.json";
+          description = ''
+            Path to the killswitch daemon JSON configuration file. This is used
+            when `settings` is null.
+          '';
+        };
+      };
+
+      config = lib.mkIf cfg.enable {
+        users.groups.killswitch = {};
+
+        environment.systemPackages = [ cfg.package ];
+
+        systemd.services.killswitch = {
+          description = "Linux eBPF network killswitch";
+          wantedBy = [ "multi-user.target" ];
+          after = [ "network-pre.target" ];
+          wants = [ "network-pre.target" ];
+          path = [ pkgs.iw ];
+          serviceConfig = {
+            Type = "simple";
+            ExecStart = lib.escapeShellArgs [ "${lib.getExe cfg.package}" (toString configPath) ];
+            Restart = "on-failure";
+            RestartSec = "2s";
+          };
+        };
+      };
+    };
+  in
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {
         inherit system;
@@ -56,7 +140,46 @@
           };
         };
       };
+
+      killswitch = pkgs.buildGoModule {
+        pname = "killswitch";
+        version = "0.1.0";
+        src = ./.;
+        vendorHash = "sha256-R6IPwurr2qMQNvMLCBNMugFCokNm1E9Jyl8W5LesPJE=";
+        proxyVendor = true;
+
+        nativeBuildInputs = [
+          bpfClang
+          llvm.llvm
+        ];
+
+        env.CGO_ENABLED = "0";
+
+        preBuild = ''
+          export CPATH="${pkgs.linuxHeaders}/include:${pkgs.libbpf}/include:$CPATH"
+          go generate ./cmd/killswitch
+        '';
+
+        subPackages = [
+          "cmd/killswitch"
+          "cmd/killswitch-cli"
+          "cmd/killswitch-user"
+        ];
+
+        meta = {
+          description = "Linux eBPF network killswitch";
+          homepage = "https://github.com/asciimoth/killswitch";
+          license = with pkgs.lib.licenses; [ mit gpl3Only ];
+          mainProgram = "killswitch";
+          platforms = pkgs.lib.platforms.linux;
+        };
+      };
     in {
+      packages = {
+        default = killswitch;
+        killswitch = killswitch;
+      };
+
       devShells.default = pkgs.mkShell {
         shellHook = checks.pre-commit-check.shellHook + ''
           export CGO_ENABLED=0
@@ -90,5 +213,9 @@
           pwru
         ];
       };
-    });
+    })
+    // {
+      nixosModules.default = module;
+      nixosModules.killswitch = module;
+    };
 }
